250087351f88930dad1dad18cbb2f002bb7fb9a1
yxing11 <yxing11@usfca.edu>
2022-04-02 16:54:38 -0700

    update

---
+++ JavaScript/array/getItem.js
@@ -0,0 +1,11 @@
+var arr = [1,2,3,4]
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+console.log(arr.indexOf(1));
+console.log(arr.indexOf(2));
+console.log(arr.indexOf(3));
+console.log(arr.indexOf(4));
