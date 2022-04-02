6f1424babdb3688fd11ec97bdd2415ade4164769
yxing11 <yxing11@usfca.edu>
2022-04-02 16:44:13 -0700

    update

---
+++ JavaScript/number/randint.js
@@ -0,0 +1,10 @@
+function randint(value) {
+  var random_val = Math.random() * value;
+  return Math.floor(random_val)
+}
+
+console.log(randint(10));
+console.log(randint(10));
+console.log(randint(10));
+console.log(randint(10));
+console.log(randint(10));
