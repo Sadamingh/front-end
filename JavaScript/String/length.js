b4ea35b33f5762bc704d6ebc681375708e8cff33
yxing11 <yxing11@usfca.edu>
2022-04-02 16:12:12 -0700

    update

---
+++ JavaScript/String/length.js
@@ -0,0 +1,3 @@
+var text = "This is a testing text script for counting its length."
+
+console.log(text.length)
---
+++ JavaScript/String/length2.js
@@ -0,0 +1,4 @@
+var text = "This is a testing text script for counting its length."
+var maxLen = 100;
+
+console.log("You have " + text.length + " written / " + (maxLen - text.length) + " left.")
