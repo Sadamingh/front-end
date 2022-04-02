be23816f54715ef95a4e079ae819564488777e71
yxing11 <yxing11@usfca.edu>
2022-04-02 16:29:39 -0700

    update

---
+++ JavaScript/number/incdec.js
@@ -0,0 +1,13 @@
+var x = 2;
+console.log(x++)
+console.log(x)
+console.log(x--)
+console.log(x)
+
+console.log("-------------")
+
+var x = 2;
+console.log(++x)
+console.log(x)
+console.log(--x)
+console.log(x)
