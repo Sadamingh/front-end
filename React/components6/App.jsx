685e175d56ffacc1038e3ccccced0fb695aa757d
yxing11 <yxing11@usfca.edu>
2022-04-28 10:56:19 -0700

    update

---
+++ React/components6/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const calculator = {
+  add: (n1, n2) => n1 + n2,
+  multiply: (n1, n2) => n1 * n2,
+  subtract: (n1, n2) => n1 - n2,
+  divide: (n1, n2) => n1 / n2
+};
+
+export default calculator;
---
+++ React/components6/components5.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import calculator from "./App";
+
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>{calculator.add(2, 3)}</li>
+      <li>{calculator.subtract(2, 3)}</li>
+      <li>{calculator.multiply(2, 3)}</li>
+      <li>{calculator.divide(2, 3)}</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
