ba9b9d51a06933d86c1715754947036493b62487
yxing11 <yxing11@usfca.edu>
2022-04-28 09:16:22 -0700

    update

---
+++ React/inlineStyling.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var fname = "Adam";
+var lname = "Edelweiss";
+var number = Math.floor(Math.random() * 10);
+
+var today = new Date();
+var currentYear = today.getFullYear();
+
+var imgURL = "https://picsum.photos/300/200";
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+if (today.getHours() > 20 || today.getHours() < 5) {
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 style={customStyle} contentEditable="true" spellCheck="false">
+      Hello {`${fname} ${lname}`}!
+    </h1>
+    <p>Your lucky number is {number}.</p>
+    <img src={imgURL} />
+    <ul>
+      <li> Hello </li>
+      <li> Hello </li>
+      <li> Hello </li>
+    </ul>
+    <footer>Copyright in {currentYear}.</footer>
+  </div>,
+  document.getElementById("root")
+);
