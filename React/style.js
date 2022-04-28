abe1aa7ad85d2c023bff87bf445475224eff90b5
yxing11 <yxing11@usfca.edu>
2022-04-28 09:09:32 -0700

    update

---
+++ React/style.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var fname = "Adam";
+var lname = "Edelweiss";
+var number = Math.floor(Math.random() * 10);
+
+var today = new Date();
+var currentYear = today.getFullYear();
+
+var imgURL = "https://picsum.photos/300/200";
+
+ReactDOM.render(
+  <div>
+    <h1 style={{ color: "red" }} contentEditable="true" spellCheck="false">
+      Hello {`${fname} ${lname}`}!
+    </h1>
+    <p>Your lucky number is {number}.</p>
+    <img src={imgURL} />
+    <ul>
+      <li> Hello </li>
+      <li> Hello </li>
+      <li> Hello </li>
+    </ul>
+    <footer>Copyright in {currentYear}.</footer>
+  </div>,
+  document.getElementById("root")
+);
