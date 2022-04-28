628bf68293b00d491723d3eb43c08f4c222e0358
yxing11 <yxing11@usfca.edu>
2022-04-28 10:37:47 -0700

    update

---
+++ React/components4/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+import Greetings from "./Greetings";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Greetings />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/components4/Greetings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import Greetings from "./Greetings";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <Greetings />
+  </div>,
+  document.getElementById("root")
+);
