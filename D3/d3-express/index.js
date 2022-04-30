fc5051721e7e9d05960bcdcf79a1dae152e10cbb
yxing11 <yxing11@usfca.edu>
2022-04-30 08:43:33 -0700

    update

--- D3/d3-express/index.js
+++ D3/d3-express/index.js
@@ -1,2 +1,17 @@
+// jshint esversion:6
+
+import path from 'path';
 import express from 'express';
-import * as d3 from 'd3';
+
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function(){
+  console.log("Server start on port 3000");
+});
---
+++ D3/d3-express/public/myd3.js
@@ -0,0 +1,4 @@
+import * as d3 from "https://cdn.skypack.dev/d3@7";
+
+d3.select(".target")
+  .style("stroke-width", 8)
